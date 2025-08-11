6f67e2a43d7490f384e6456fafa2dc5263ba44e5
ccrd1711 <ccrd17@gmail.com>
2025-08-11 20:56:51 +0100

    add landing page at / and move gallery to /gallery/ with updated template, home.html, and url

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,4 @@
-from django.http import JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Gallery, Photo 
 
-# Create your views here.
-def my_gallery(request):
-    galleries = Gallery.objects.all().order_by("-id")
-    return render(request, "gallery/index.html", {"galleries": galleries})
+def index(request):
+    return render(request, "gallery/index.html")
--- vixi_photography/urls.py
+++ vixi_photography/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("gallery.urls")),
+    path("", home, name="home"), #landing page?
+    path("gallery/", include("gallery.urls")), #gallery page?
 ]
