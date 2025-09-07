f38a7734d4098f79d1e84d57be5e96a6559655eb
ccrd1711 <ccrd17@gmail.com>
2025-09-07 12:25:43 +0100

    Amend gallery views due to gallery error on full deployment. Working locally so test to push

--- gallery/views.py
+++ gallery/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Photo
 
 def index(request):
-    photos = Photo.objects.order_by('-uploaded_at')
+    photos = Photo.objects.order_by('-id') 
     return render(request, "gallery/gallery.html", {"photos": photos})
