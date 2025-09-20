187eef9585bd6050c6d3b6c1627a8092147a21b4
ccrd1711 <ccrd17@gmail.com>
2025-09-20 18:36:41 +0100

    Add template view due to error on deployment. Add and push ot test

--- vixi_photography/urls.py
+++ vixi_photography/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
